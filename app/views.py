11d4d24c9c47ac23a7d4ef4344007fc4f0084fe4
Shanice A. Bryan <indi.shanbry@gmail.com>
2017-04-04 01:44:08 +0000

    Ex2 Completed

--- app/views.py
+++ app/views.py
@@ -11,6 +11,8 @@ from bs4 import BeautifulSoup
 import requests
 import urlparse
 
+from imageGetter import get_imgsList
+
 ###
 # Routing for your application.
 ###
@@ -20,6 +22,20 @@ def home():
     """Render website's home page."""
     return render_template('home.html')
 
+
+@app.route('/api/thumbnails', methods=['GET'])
+def get_thumbnails():
+    url = "https://www.amazon.com/dp/B019U00D7K"
+    links = get_imgsList(url)
+    
+    if links:
+        xxx = None
+        statusMsg = "Success"
+    else:
+        xxx = "Error in Request"
+        statusMsg = "Fail"
+    
+    return jsonify(error=xxx, message=statusMsg, thumbnails=links)
 
 ###
 # The functions below should be applicable to all Flask apps.
